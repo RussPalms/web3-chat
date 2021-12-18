9c39b11a972a8f3d61153e245d842fcd03e80b37
Russell Palma <rpalm@DUGADUGA.local>
2021-12-17 16:28:25 -0800

    finished build

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Login from "./components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "./components/Header";
+import Messages from "./components/Messages";
 
 export default function Home() {
 	const { isAuthenticated, logout } = useMoralis();
@@ -10,11 +12,16 @@ export default function Home() {
 	}
 
 	return (
-		<div className="h-screen">
+		<div className="h-screen overvlow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
 			<Head>
 				<title>Metaverse Chat</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+
+			<div className="max-w-screen-2xl mx-auto">
+				<Header />
+				<Messages />
+			</div>
 
 			<button onClick={logout}>Logout</button>
 		</div>
